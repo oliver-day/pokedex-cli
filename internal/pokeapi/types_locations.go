38609c27fd2aa5b7cb7c08c31863049fe6175ec7
Oliver Day <oliver.day.swe@gmail.com>
2024-05-11 13:58:42 -0700

    feat: add 'RespShallowLocations' type

---
+++ internal/pokeapi/types_locations.go
@@ -0,0 +1,12 @@
+package pokeapi
+
+// RespShallowLocations is the response from the locations endpoint
+type RespShallowLocations struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results`
+}
