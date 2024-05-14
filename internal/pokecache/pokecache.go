04aeee4a040c230349db9809d2da0e622ce9f66d
Oliver Day <oliver.day.swe@gmail.com>
2024-05-13 20:41:25 -0700

    feat: add 'Cache' struct

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,18 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+// Cache
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
